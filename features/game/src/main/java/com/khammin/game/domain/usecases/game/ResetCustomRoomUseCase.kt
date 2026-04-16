ebf7ededfeeaa8582a03cd2b7282d55542e9366e
etharalrehaili <85852139+etharalrehaili@users.noreply.github.com>
2026-04-16 17:09:50 +0300

    commit

---
+++ features/game/src/main/java/com/khammin/game/domain/usecases/game/ResetCustomRoomUseCase.kt
@@ -0,0 +1,10 @@
+package com.khammin.game.domain.usecases.game
+
+import com.khammin.game.domain.repository.MultiplayerRepository
+import javax.inject.Inject
+
+class ResetCustomRoomUseCase @Inject constructor(
+    private val repo: MultiplayerRepository
+) {
+    suspend operator fun invoke(roomId: String) = repo.resetCustomRoom(roomId)
+}
